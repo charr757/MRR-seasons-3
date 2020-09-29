523608b251d49b67ac9ea3070b1121206600b6f7
Charlie Spencer <crs992+charr757@gmail.com>
2020-09-28 21:38:15 -0400

    geolocation console log

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 const App = () => {
+  window.navigator.geolocation.getCurrentPosition(
+    (position) => console.log(position),
+    (err) => console.log(err)
+  )
   return(
-    <div>Hi there!</div>
+    <div>Latitude: </div>
   );
 }
 
