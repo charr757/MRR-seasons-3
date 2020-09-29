5b182c36ec42e3b12e46bb4b9c68be0c66609e83
Charlie Spencer <crs992+charr757@gmail.com>
2020-09-28 22:02:13 -0400

    initializing state

--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
+  // Special function, not required by ReactDOM, first function that will be called everytime there is an instance of this class
+  // Good time to initialize state
+  constructor(props) {
+    super(props); // reference to the parents constructor function
+
+    this.state = { lat: null };
+  }
+
+  // React says we have to define render!! Otherwise, it'll get angry
   render() {
     window.navigator.geolocation.getCurrentPosition(
       (position) => console.log(position),
       (err) => console.log(err)
     );
-    
+
     return <div>Latitude: </div>
   }
 }
